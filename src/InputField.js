63285c980ef7ab9432528e63ce6cfd3479f30e97
Kunkun <jikunyang03@gmail.com>
2022-03-22 20:28:25 +1300

    added a label text and submit button which handles all state value

--- src/InputField.js
+++ src/InputField.js
@@ -8,11 +8,17 @@ export default class InputField extends React.Component {
         //console.log(event.target.value);
         this.setState({ name: event.target.value});
     }
+    handleSubmit = () =>{
+        console.log(this.state);
+    }
     render(){
         return (
             <div>
+                <p>
+                   Enter your name here. We don't steal data. 
+                </p>
               <input value={this.state.name} onChange={this.handleChange} />
-              
+              <button onClick={this.handleSubmit}>submit</button>
             </div>
           );
     }
